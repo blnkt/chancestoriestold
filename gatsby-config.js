d7378b76d04c4c389a4728a9ac10f9a15c6cf547
blnkt <blnktd@gmail.com>
2019-02-07 20:41:22 -0800

    Add support for third-party GraphQL API requests

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain the remote schema Query type
+        typeName: "SWAPI",
+        // This is the field under which it's accessible
+        fieldName: "swapi",
+        // URL to query from
+        url: "https://api.graphcms.com/simple/v1/swapi",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
